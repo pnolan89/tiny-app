f698b827eacedba90e74243f71f2ca23d2bdef0d
pnolan89 <petenol@gmail.com>
2019-01-16 22:41:31 +0000

    added login/logout feature

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
@@ -19,26 +21,43 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.login);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase};
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   console.log(urlDatabase);
-  res.redirect(`http://localhost:8080/urls/${shortURL}`);
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
@@ -47,12 +66,12 @@ app.post("/urls/:id", (req, res) => {
   let longURL = req.body.url;
   let shortURL = req.params.id;
   urlDatabase[shortURL] = longURL;
-  res.redirect(`http://localhost:8080/urls/${shortURL}`);
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
-  res.redirect("http://localhost:8080/urls/");
+  res.redirect("/urls/");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -61,7 +80,9 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/u/:id", (req, res) => {
   let longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  longURL = longURL.replace("http://", "");
+  longURL = longURL.replace("www.", "");
+  res.redirect(`http://www.${longURL}`);
 });
 
 app.listen(PORT, () => {
